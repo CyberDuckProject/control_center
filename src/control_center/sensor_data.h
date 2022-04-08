94537040eba0f35412d637fb2fc52f6951870647
Jan Bielak <contact@janbielak.com>
2022-04-08 17:16:44 +0200

    Auto-adjust min and max graph values

--- src/control_center/sensor_data.h
+++ src/control_center/sensor_data.h
@@ -3,6 +3,7 @@
 
 #include "ringbuffer.h"
 #include <iostream>
+#include <limits>
 
 enum class SensorType {
   BEGIN = 1,
@@ -22,49 +23,24 @@ public:
       static_cast<size_t>(SensorType::END) -
       static_cast<size_t>(SensorType::BEGIN);
   inline static constexpr size_t buffer_size = 120;
+  SensorData() {
+    std::fill_n(min_vals, sensor_count, 0);
+    std::fill_n(max_vals, sensor_count, 0);
+  }
   void add_reading(SensorType sensor, int64_t /*time*/, float reading) {
-    data[static_cast<size_t>(sensor) - 1].push_back(reading);
+    const auto idx = static_cast<size_t>(sensor) - 1;
+    min_vals[idx] = std::min(min_vals[idx], reading);
+    max_vals[idx] = std::max(max_vals[idx], reading);
+    data[idx].push_back(reading);
   }
   const Ringbuffer<float, buffer_size> operator[](size_t idx) const noexcept {
     return data[idx];
   }
   float min_val(size_t idx) const noexcept {
-    switch (idx) {
-    case 0:
-      return 20.0f;
-    case 1:
-      return 0.0f;
-    case 2:
-      return 80.0f;
-    case 3:
-      return 100700.0f;
-    case 4:
-      return 23.0f;
-    case 5:
-      return 31.0f;
-    case 6:
-      return 0.0f;
-    }
-    return 0.0f;
+    return min_vals[idx];
   }
   float max_val(size_t idx) const noexcept {
-    switch (idx) {
-    case 0:
-      return 22.0f;
-    case 1:
-      return 0.0f;
-    case 2:
-      return 105.0f;
-    case 3:
-      return 100800.0f;
-    case 4:
-      return 24.0f;
-    case 5:
-      return 32.0f;
-    case 6:
-      return 0.0f;
-    }
-    return 0.0f;
+    return max_vals[idx];
   }
   const char *name(size_t idx) const noexcept {
     switch (idx) {
@@ -88,6 +64,8 @@ public:
 
 private:
   Ringbuffer<float, buffer_size> data[sensor_count];
+  float min_vals[sensor_count];
+  float max_vals[sensor_count];
 };
 
 #endif
